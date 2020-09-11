681f1a837cef717c59f82400a3148932729dbde0
jahanzab <jfarooq@mgc.mentorg.com>
2020-09-11 12:48:05 +0500

    updated test case

--- src/test/java/grid_testing/gridtest.java
+++ src/test/java/grid_testing/gridtest.java
@@ -5,6 +5,7 @@ import java.net.URL;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,33 +16,13 @@ import org.testng.annotations.Test;
 public class gridtest {
 
 	public WebDriver driver;
-	DesiredCapabilities dc = new DesiredCapabilities();
 
-	@Parameters({ "browser" })
 	@Test
-	public void remote_test(String browser) throws MalformedURLException {
+	public void remote_test() throws MalformedURLException {
 
-		if (browser.equals("chrome")) {
-			System.out.println("Inside Chrome...");
-			dc.setPlatform(Platform.ANY);
-			dc.setBrowserName("chrome");
-			ChromeOptions options1 = new ChromeOptions();
-			options1.merge(dc);
-		}
+		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+		driver = new ChromeDriver();
 
-		else if (browser.equals("firefox")) {
-			System.out.println("Inside Firefox...");
-			dc.setPlatform(Platform.ANY);
-			dc.setBrowserName("firefox");
-			FirefoxOptions options = new FirefoxOptions();
-			options.merge(dc);
-		}
-		
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), dc);
 		driver.get("https://www.google.com/maps");
-		
-
-		System.out.println("Thats all of it");
 	}
-
 }
